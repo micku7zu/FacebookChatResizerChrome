459a2f171b7bb5b505783afe161cd957e58f2885
micku7zu <micku7zu@gmail.com>
2015-01-17 01:36:12 +0200

    Partial rebase

--- javascript/extension.js
+++ javascript/extension.js
@@ -1,15 +1,33 @@
-var clicked = 0;
 var tX = 0, tY = 0;
 var current;
 
+var bodyHeightDif = 55; //the difference between the window and the body with the text
+var bodyMinWidth = 200;
+var bodyMinHeight = 200;
+var bodyMaxWidth = 600;
+var bodyMaxHeight = 600;
+
+var defaultWidth = 0,
+	defaultHeight = 0; //default width and height of a window, we will get it
+
+//on windows load
 window.addEventListener("load", function() {
 
-	setInterval(function() {
-		insertResizeButton();
-	}, 1000);
+	//try to insert the resize button
+	setInterval(function() { insertResizeButton(); }, 1000);
 
-	$(document.body).on('click', '.extresize',function(){
-		onClick($(this));
+	//on resize button mousedown and up (click, hold, move, release)
+	$(document.body).on('mousedown', '.extresize', function(){
+		onClickPressed($(this));
+	}).on('mouseup', '.extresize', function() {
+		onClickReleased($(this));
+	});
+
+	$('.drag').drag(function( ev, dd ){
+	$( this ).css({
+	top: dd.offsetY,
+	left: dd.offsetX
+	});
 	});
 
 }, false);
@@ -18,67 +36,86 @@ function insertResizeButton() {
 	var elems = $(".mls.titlebarButtonWrapper");
 	elems.each(function(el) {
 		if($(this).children(".extresize").length == 0) {
+			//add the resize button
 			$(this).prepend('<a style="background-image:url( ' + chrome.extension.getURL('images/resize.png') + ');" class="extresize close button" data-ft="{&quot;tn&quot;:&quot;+\u003C&quot;}" role="button" tabindex="0" data-hover="tooltip" aria-label="Resize"></a>');
+		
+			//hide the annoying info box
+			var chatBox = $(this).parents(".fbDockChatTabFlyout").eq(0);
+			chatBox.find('.fbNubFlyoutHeader').eq(0).hide();
+
+			//get a default width if not set
+			if(defaultWidth == 0) {
+				defaultWidth = chatBox.width();
+				defaultHeight = chatBox.height();
+
+				console.log("Default width and height: " + defaultWidth + " - " + defaultHeight);
+			}
+
+			//resize the window after we removed the info box
+			resize(defaultWidth, defaultHeight, $(this));
 		}
 	});
 }
 
 
-function resize(w, h) {
-	elem = current;
-	var megaParent = elem.parents(".fbNub").eq(0);
-	var bigBox = elem.parents(".fbDockChatTabFlyout").eq(0);
-	var bodyHeight = h - 55;
+function resize(w, h, elem) {
+	if(elem == undefined) {
+		elem = current;
+	}
 
-	megaParent.css("width", w + "px");
-	bigBox.css("width", w + "px");
-	bigBox.css("height", h + "px");
-	var body = $(bigBox).find(".fbNubFlyoutBody").eq(0);
-	body.css("height", bodyHeight + "px");
+	var parent = elem.parents(".fbNub").eq(0);
+	var chatBox = elem.parents(".fbDockChatTabFlyout").eq(0);
+	var chatBody = $(chatBox).find(".fbNubFlyoutBody").eq(0);
+
+	var bodyHeight = h - bodyHeightDif; //height of the chatBody
+
+	parent.css("width", w + "px");
+	chatBox.css("width", w + "px");
+	chatBox.css("height", h + "px");
+	chatBody.css("height", bodyHeight + "px");
 }
 
-function onMove(e) {
-	clicked = 1;
-	var w = tX - e.clientX;
-	var h = tY - e.clientY;
+function onMouseMove(e) {
+	var w = tX - e.clientX,
+		h = tY - e.clientY;
 
-	if(w < 200) {
-		w = 200;
-	}
-
-	if(h < 200) {
-		h = 200;
-	}
+	if(w < bodyMinWidth) w = bodyMinWidth;
+	if(w > bodyMaxWidth) w = bodyMaxWidth;
+	if(h < bodyMinHeight) h = bodyMinHeight;
+	if(h > bodyMaxHeight) h = bodyMaxHeight;
 
 	resize(w, h)
-
 }
 
-function onClick(elem) {
-	var megaParent = elem.parents(".fbNub").eq(0);
-	var bigBox = elem.parents(".fbDockChatTabFlyout").eq(0);
-	var info = bigBox.find('.fbNubFlyoutHeader').eq(0);
-	var body = $(bigBox).find(".fbNubFlyoutBody").eq(0);
-	var nustiu = $(bigBox).find(".fbNubFlyoutInner").eq(0);
+function onClickPressed(elem) {
+	var parent = elem.parents(".fbNub").eq(0);
+	var chatBox = elem.parents(".fbDockChatTabFlyout").eq(0);
+	var chatBody = $(chatBox).find(".fbNubFlyoutBody").eq(0);
 
-	if(clicked == 0) {
-		info.hide();
+	tX = chatBox.offset().left + chatBox.width();
+	tY = chatBox.offset().top - $(window).scrollTop() + chatBox.height();
 
-		tX = bigBox.offset().left + bigBox.width();
-		tY = bigBox.offset().top - $(window).scrollTop() + bigBox.height();
+	//chatBody.find(".conversation").eq(0).css("paddingTop", "500px");
+	//chatBody.animate({"scrollTop": 99999}, 0);
+	//chatBody.css("overflow-y", "scroll");
 
-		body.find(".conversation").eq(0).css("paddingTop", "500px");
-		body.animate({"scrollTop": 99999}, 0);
-		body.css("overflow-y", "scroll");
-		body.find("div").css("width", "auto");
-		clicked = 1;
-		current = elem;
+	//set width auto
+	chatBody.find("div").css("width", "auto");
 
-		$(document).on("mousemove", onMove);
+	//set current chat window
+	current = elem;
 
+	toggleOnMoveEvent(true); //set move mouse listener
+}
+
+function onClickReleased(elem) {
+	toggleOnMoveEvent(false); //unset move mouse listenere
+}
+
+function toggleOnMoveEvent(enabled) {
+	if(enabled) {
+		$(document).on("mousemove", onMouseMove);
 	}else{
-		body.animate({"scrollTop": 99999}, 0);
-		clicked = 0;
-		$(document).off("mousemove", onMove);
+		$(document).off("mousemove", onMouseMove);
 	}
 }
